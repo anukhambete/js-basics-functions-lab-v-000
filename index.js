7b877b4543e14440e1f828497c3816a0c7552ce8
Anu Khambete <anukhambete@gmail.com>
2018-11-07 01:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function calculatesFarePrice (start, destination) {
 
   if (dist <= 400) {
     return 0;
-  } else if (dist > 400 && dist < 2000) {
+  } else if (dist > 400 && dist <= 2000) {
     return (dist-400) * 0.02;
+  } else if (dist > 2000 && dist < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
 }
