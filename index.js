43b5f5f7fc4cc939d9485fcfc1556480db9fa47b
Anu Khambete <anukhambete@gmail.com>
2018-11-07 01:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function distanceTravelledInFeet (a,b) {
 }
 
 calculatesFarePrice (start, destination) {
-  const dist = function distanceTravelledInFeet(start, destination);
+    const dist = function distanceTravelledInFeet(start, destination);
 
     if (dist <= 400) {
       return 0;
