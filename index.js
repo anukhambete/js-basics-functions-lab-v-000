462706678bb0169f4829fd9e01de1200bcbdfa4c
Anu Khambete <anukhambete@gmail.com>
2018-11-07 01:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function distanceTravelledInFeet (a,b) {
 }
 
 function calculatesFarePrice (start, destination) {
-  const distance = distanceTravelledInFeet(start, destination);
+  const dist = distanceTravelledInFeet(start, destination);
 
-  if (distance <= 400) {
+  if (dist <= 400) {
     return 0;
-  } else if (distance > 400 && distance <= 2000) {
-    return .02 * (distance - 400);
-  } else if (distance > 2000 && distance < 2500) {
+  } else if (dist > 400 && dist <= 2000) {
+    return .02 * (dist - 400);
+  } else if (dist > 2000 && dist < 2500) {
     return 25;
   } else {
     return 'cannot travel that far';
