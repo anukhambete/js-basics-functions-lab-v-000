784cec8d65c961d22076dec5f79a592a7e503d5f
Anu Khambete <anukhambete@gmail.com>
2018-11-07 01:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function distanceFromHqInFeet (block) {
 function distanceTravelledInFeet (a,b) {
   return (Math.abs(a-b) * 264);
 }
+
+calculatesFarePrice (start, destination) {
+  let dist = function distanceTravelledInFeet (start, destination)
+}
