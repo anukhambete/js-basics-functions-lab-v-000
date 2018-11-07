b642cf33d6033fce02a7fb49050712cb90c4227e
Anu Khambete <anukhambete@gmail.com>
2018-11-07 01:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ calculatesFarePrice (start, destination) {
     if (dist <= 400) {
       return 0;
     } else if (dist > 400 && dist <= 2000) {
-
+      return dist * 0.02;
     } else if (dist > 2000 && dist < 2500) {
       return 'I will gladly take your thirty bucks.';
     } else if (dist > 2000) {
